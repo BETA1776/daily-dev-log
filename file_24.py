91bbcebfb60dcb47eb6ef52a65ed592867768e4a
Krish Rajput <krishrajput1776cosmic@gmail.com>
2026-07-22 10:48:21 +0530

    updated notes after learning

---
+++ file_24.py
@@ -0,0 +1,14 @@
+
+# 2026-07-22 10:48:21.029687
+def binary_search(arr, target):
+    low, high = 0, len(arr)-1
+    while low <= high:
+        mid = (low+high)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid+1
+        else:
+            high = mid-1
+    return -1
+
